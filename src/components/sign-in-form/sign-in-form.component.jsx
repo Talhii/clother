ff14908b67aeb1e05de62b08dcb930d78436b9b3
alihassan7111 <alihassan@rightfarm.com>
2023-03-06 18:26:27 +0500

    A commit

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -69,7 +69,7 @@ const SignInForm = () => {
         <div className='buttons-container'>
           <Button type='submit'>Sign In</Button>
           <Button buttonType='google' type='button' onClick={signInWithGoogle}>
-            SignIn With Google
+            Sign In With Google
           </Button>
         </div>
       </form>
