f5d2b5c286fbfe2e9f3d42d4a4ab93b8642edc06
alihassan7111 <alihassan@rightfarm.com>
2023-03-07 17:35:44 +0500

    Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.scss';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
-import './index.scss';
-
-const rootElement = document.getElementById('root');
-
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
@@ -22,6 +20,7 @@ render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
+
+reportWebVitals();
